305b468306f0140a43268841473f89d47fd159f2
Cedar Li <china.cedar.li@gmail.com>
2013-03-05 17:30:52 +0800

    [modify] 10.4.c
    Signed-off-by: Cedar Li <china.cedar.li@gmail.com>

--- 10.4.c
+++ 10.4.c
@@ -1,17 +1,21 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 int sig_int_flag = 0; /*set nonzero when fignal occurs*/
 static void sig_int(int);/*my signal handling function*/
 int main(){
     signal(SIGINT,sig_int);
     printf("signal in main\n");
-    while (sig_int_flag == 0)
-        pause();
+    while (sig_int_flag == 0){
+        kill(getpid(),SIGINT);//??????????????????
+        pause();   /*signal happen between check sig_int_flag and pause*/
+    }
+
 }
 
 static void sig_int(int sig_no){
     signal(SIGINT,sig_int);
-    printf("\nsignal in sig_inti\n");
+    printf("\nsignal in sig_int\n");
     sig_int_flag = 1;
 }
