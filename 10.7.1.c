7ea36ea682f2fa39aeb94bc48858c33b011dbc92
Cedar Li <china.cedar.li@gmail.com>
2013-03-12 16:19:35 +0800

    [add] 10.10.1.c 10.10.2.c 10.10.3.c 10.10.4.c 10.7.1.c 10.9.1.c 10.9.1.raise.c.
    Signed-off-by: Cedar Li <china.cedar.li@gmail.com>

---
+++ 10.7.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+static void sig_cld(int);
+int main(){
+    pid_t pid;
+    if ( signal(SIGCLD,SIG_IGN) == SIG_ERR )
+        printf("signal error");
+    printf("parent pid = %d\n",getpid());
+    if ( (pid = fork())<0 ) {
+        printf("fork error");
+    }else if (pid == 0 ){   /*child*/
+        printf("child pid = %d\n",getpid());
+        printf("before sleep\n");
+        sleep(2);
+        printf("end sleep\n");
+        _exit(0);
+    }
+    pause();    /*parent*/
+    exit(0);
+}
