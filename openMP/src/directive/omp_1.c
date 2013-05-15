c7e90551873f1a42af5c45fc9ceae48d4fb11c07
Cedar Li <china.cedar.li@gmail.com>
2013-05-15 10:05:06 +0800

    [modify] omp_1.c
    Signed-off-by: Cedar Li <china.cedar.li@gmail.com>

--- openMP/src/directive/omp_1.c
+++ openMP/src/directive/omp_1.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <omp.h>
 int main(int argc, char* argv[]) {
     int i;
+    int j=1;
 
-#pragma omp parallel for
+#pragma omp parallel for num_threads(4)
     for ( i = 0; i < 10; i++ ){
-        printf("i = %d\n", i);
+        printf("i = %d omp_thread=%d\n", i,omp_get_thread_num());
     }
     return 0;
 }
